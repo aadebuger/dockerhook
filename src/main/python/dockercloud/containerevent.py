b108e319e06f8d1041523e6dd1f39e4ae959266c
aadebuger <admin@local.host>
2015-01-03 00:27:45 +0800

    inspect

--- src/main/python/dockercloud/containerevent.py
+++ src/main/python/dockercloud/containerevent.py
@@ -5,10 +5,12 @@ Created on Jan 2, 2015
 '''
 from docker import Client
 import json
-def processEvent(eventstr):
+def processEvent(cli,eventstr):
     event = json.loads(eventstr)
     print 'id=',event['id']
     print 'status=',event['status']
+    info = cli.inspect_container(event['id'])
+    print 'info=',info
 def events():
     cli = Client(base_url='tcp://127.0.0.1:4243')
     eventiter = cli.events()
@@ -16,7 +18,7 @@ def events():
     while 1:
             event = eventiter.next()
             print 'event',event
-            processEvent(event)
+            processEvent(cli,event)
         
 if __name__ == '__main__':
     events();
