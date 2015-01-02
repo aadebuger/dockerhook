49496d5e3d5951c933aa026742e7678d7b9b48cf
aadebuger <admin@local.host>
2015-01-02 23:56:38 +0800

    first commit

---
+++ src/main/python/dockercloud/containerevent.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 2, 2015
+
+@author: aadebuger
+'''
+from docker import Client
+
+def events():
+    cli = Client(base_url='tcp://127.0.0.1:4243')
+    eventiter = cli.events
+    for event in eventiter:
+        print 'event',event
+        
+if __name__ == '__main__':
+    pass
